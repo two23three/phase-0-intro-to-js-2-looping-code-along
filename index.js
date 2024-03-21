29f5ce0995d35a41f6b2542d1ed372bbdfc75640
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-21 20:54:59 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const namesArray =["Tulley","frenchie","Mary"];
+
+const writeCards=(namesArray,event)=>{
+    let thanksCards=[]
+    for (let i=0; i< namesArray.length; i++ ){
+        thanksCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+   return thanksCards
+    
+}
+const event = "birthday";
+const cards = writeCards(namesArray, event);
+console.log(cards);
+
+const countDown =(startingNo) =>{
+    while (startingNo> 0){
+        console.log(startingNo);
+        startingNo-=1;
+    }
+    console.log(startingNo);
+
+}
